6b384fdb22908c551277377062c4ac0637138091
vojtech <sopajzyt@gmail.com>
2021-04-27 22:59:34 +0200

    literally nothing works yet

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,16 @@
+
+<template>
+    <div id="mainDiv">
+        <img src="@/assets/images/MangaKatLogo.png" alt="">
+    </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Main'
+}
+</script>
+
+
+<style scoped src="@/assets/css/navbar.css"/>
---
+++ frontend/src/views/Main.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="mainDiv">
+        <div id="downloadDiv">
+            <input type="text">
+            <a v-on:click="sendData()">
+                <img src="@/assets/images/MangaKatLogo.png" alt="">
+            </a>
+        </div>
+    </div>
+</template>
+
+
+<script>
+
+import axios from 'axios'
+
+export default {
+    name: "Main",
+    
+    // mounted aby se to dalo hned po nasteni asi
+    methods: {
+        sendData: function(){
+            axios({
+            method: 'post',
+            url: 'http://localhost:5000',
+            data: {
+                firstName: 'Finn',
+                lastName: 'Williams'
+            }
+            });
+        }
+    }
+}
+</script>
+
+
+
+
+<style scoped src="@/assets/css/main.css"/>
